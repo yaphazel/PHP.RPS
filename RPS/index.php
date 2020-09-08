de7e904927f1f2b7fe3a956ab0e373bf0d3bee49
yaphazel <hazelyap98@gmail.com>
2020-09-08 16:28:20 +0800

    yay

--- RPS/index.php
+++ RPS/index.php
@@ -1,16 +1,18 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>HAZEL PIK YEN YAP 12a9d091 | Rock Paper Scissors</title>
-    <style media="screen">
-      h1{
-        font-family:  Times;
-      }
-    </style>
+    <title>HAZEL PIK YEN YAP | Rock Paper Scissors</title>
+    <link rel="stylesheet" href="style2.css">
   </head>
   <body>
-      <h1>Welcome to Rock Paper Scissors</h1>
-      <a href="login.php">Please Log In</a>
+    <h1>Welcome to Rock Paper Scissors</h1>
+    <div class="container">
+      <p>
+        <img src="RPS.png" height=200px class="indeximg"> <!-- Image found in https://towardsdatascience.com/building-a-rock-paper-scissors-ai-using-tensorflow-and-opencv-d5fc44fc8222-->
+      </p>
+      <p><a href="login.php">Please Log In</a></p>
       <p>Attempt to go to <a href="game.php">game.php</a> without logging in - it should fail with an error message.</p>
+    </div>
+
   </body>
 </html>
--- RPS/login.php
+++ RPS/login.php
@@ -8,7 +8,7 @@ $message='';
 if (isset($_POST['pass']) && isset($_POST['who'])){
 
   if (strlen($_POST['pass']) < 1 || strlen($_POST['who']) < 1){
-          $message="User name and password are required";
+          $message="*User name and password are required";
         } else {
           $password = $_POST['pass'];
           $salt = 'XyZzy12*_';
@@ -30,32 +30,24 @@ if (isset($_POST['pass']) && isset($_POST['who'])){
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>HAZEL PIK YEN YAP 12a9d091 | Rock Paper Scissors</title>
-    <style media="screen">
-      h1{
-        font-family:  Times;
-      }
-      #htmlent{
-        color:red;
-      }
-      input{
-        margin:4px;
-      }
-    </style>
+    <title>HAZEL PIK YEN YAP | Rock Paper Scissors</title>
+    <link rel="stylesheet" href="style2.css">
   </head>
   <body>
-      <h1>Please Log In</h1>
-      <p id="htmlent"><?= htmlentities($message); ?></p>
-      <form action="login.php" method="post">
-        <label for="who">User Name</label>
-        <input type="text" name="who" > <br>
-        <label for="pass">Password</label>
-        <input type="password" name="pass"> <br>
-        <input type="submit" name="login" value="Log In">
-        <input type="submit" name="cancel" value="Cancel">
-        <p>For a password hint, view source and find a password hint in the HTML comments. <!-- Hint: The password is the three character name of the
-programming language used in this class (all lower case)
-followed by 123. --></p>
-      </form>
+      <h1>Rock Paper Scissors</h1>
+      <div class="form-container">
+        <h2>User Login</h2>
+        <p id="htmlent"><?= htmlentities($message); ?></p>
+        <form action="login.php" method="post">
+          <input type="text" name="who" placeholder="Username" > <br>
+          <input type="password" name="pass" placeholder="Password"> <br>
+          <input class="button" type="submit" name="login" value="LOGIN"> <br>
+          <input class="button" type="submit" name="cancel" value="CANCEL">
+          <p>For a password hint, view source and find a password hint in the HTML comments. <!-- Hint: The password is the three character name of the
+  programming language used in this class (all lower case)
+  followed by 123. --></p>
+        </form>
+      </div>
+
   </body>
 </html>
